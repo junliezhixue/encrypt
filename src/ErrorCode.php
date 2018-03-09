67fbcf7f668fb7fcdc6ce6dc32c063e2ebc3e0e8
qidongbo <qidb@jvtd.cn>
2018-03-09 16:59:08 +0800

    提交

--- src/ErrorCode.php
+++ src/ErrorCode.php
@@ -20,19 +20,18 @@ namespace wxencrypt;
  */
 class ErrorCode
 {
-	public static $OK = 0;
-	public static $ValidateSignatureError = -40001;
-	public static $ParseXmlError = -40002;
-	public static $ComputeSignatureError = -40003;
-	public static $IllegalAesKey = -40004;
-	public static $ValidateAppidError = -40005;
-	public static $EncryptAESError = -40006;
-	public static $DecryptAESError = -40007;
-	public static $IllegalBuffer = -40008;
-	public static $EncodeBase64Error = -40009;
-	public static $DecodeBase64Error = -40010;
-	public static $GenReturnXmlError = -40011;
-	public static $XmlParamError = -40012;
+	public static $ValidateSignatureError  		= [40001, '签名验证错误'];
+	public static $ParseXmlError 				= [40002, 'xml解析失败'];
+	public static $ComputeSignatureError 		= [40003, 'sha加密生成签名失败'];
+	public static $IllegalAesKey 				= [40004, 'encodingAesKey非法'];
+	public static $ValidateAppidError 			= [40005, 'appid校验错误'];
+	public static $EncryptAESError 				= [40006, 'aes加密失败'];
+	public static $DecryptAESError 				= [40007, 'aes解密失败'];
+	public static $IllegalBuffer 				= [40008, '解密后得到的buffer非法'];
+	public static $EncodeBase64Error 			= [40009, 'base64加密失败'];
+	public static $DecodeBase64Error 			= [40010, 'base64解密失败'];
+	public static $GenReturnXmlError 			= [40011, '生成xml失败'];
+	public static $XmlParamError 				= [40012, 'xml参数错误'];
 }
 
 ?>
